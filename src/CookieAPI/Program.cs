e22a651e481548ad9189dea67d98616d6ace1ad7
EnesKllokoqi1 <eneskllokoqi@gmail.com>
2026-03-25 23:21:11 +0100

    Add NuGet packages

--- src/CookieAPI/Program.cs
+++ src/CookieAPI/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,10 +14,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
