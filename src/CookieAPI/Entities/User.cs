66592b9ff1feae5a138a6971d9f014d69322ac52
EnesKllokoqi1 <eneskllokoqi@gmail.com>
2026-03-27 23:23:38 +0100

    implement database setup

---
+++ src/CookieAPI/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace CookieAPI.Entities
+{
+    public class User
+    {
+        public Guid Guid { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PasswordHash { get; set; }
+         public char Gender { get; set; }
+
+    }
+}
--- src/CookieAPI/Program.cs
+++ src/CookieAPI/Program.cs
@@ -1,9 +1,18 @@
+using CookieAPI.Data;
+using DotNetEnv;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
+
+Env.Load();
 var builder = WebApplication.CreateBuilder(args);
+var connection = Environment.GetEnvironmentVariable("DB_CONNECTION") ??
+    builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(e=>e.UseNpgsql(connection));
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
