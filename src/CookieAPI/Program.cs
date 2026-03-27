00fa97f6532328d67952b80d96d70e133d1ecf2e
EnesKllokoqi1 <eneskllokoqi@gmail.com>
2026-03-27 23:23:38 +0100

    implement database setup

--- src/CookieAPI/Program.cs
+++ src/CookieAPI/Program.cs
@@ -3,7 +3,10 @@ using DotNetEnv;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+<<<<<<< HEAD
 using Microsoft.IdentityModel.Tokens;
+=======
+>>>>>>> eacc991 (implement database setup)
 using Scalar.AspNetCore;
 using System.Text;
 
