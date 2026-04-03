2b320ce96f4d00658e685137618d4070a3457f83
EnesKllokoqi1 <eneskllokoqi@gmail.com>
2026-04-03 22:52:14 +0200

    added extra properties

--- src/CookieAPI/Entities/User.cs
+++ src/CookieAPI/Entities/User.cs
@@ -3,11 +3,13 @@
     public class User
     {
         public Guid Guid { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string EmailAddress { get; set; }
-        public string PasswordHash { get; set; }
-         public char Gender { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string EmailAddress { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiryTime { get; set; }
+        public char? Gender { get; set; }
 
     }
 }
---
+++ src/CookieAPI/Migrations/20260403204923_Added Extra Properties.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CookieAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedExtraProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<char>(
+                name: "Gender",
+                table: "User",
+                type: "character(1)",
+                nullable: true,
+                oldClrType: typeof(char),
+                oldType: "character(1)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "User",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiryTime",
+                table: "User",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiryTime",
+                table: "User");
+
+            migrationBuilder.AlterColumn<char>(
+                name: "Gender",
+                table: "User",
+                type: "character(1)",
+                nullable: false,
+                defaultValue: '\0',
+                oldClrType: typeof(char),
+                oldType: "character(1)",
+                oldNullable: true);
+        }
+    }
+}
