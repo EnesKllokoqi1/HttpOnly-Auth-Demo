72e14b7a6b0dd32fd0d843a0c2db3ed0476be0dd
EnesKllokoqi1 <eneskllokoqi@gmail.com>
2026-04-05 23:12:10 +0200

    add age property

--- src/CookieAPI/Entities/User.cs
+++ src/CookieAPI/Entities/User.cs
@@ -7,6 +7,7 @@
         public string LastName { get; set; } = string.Empty;
         public string EmailAddress { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
+        public int Age { get; set; }
         public string? RefreshToken { get; set; }
         public DateTime? RefreshTokenExpiryTime { get; set; }
         public char? Gender { get; set; }
---
+++ src/CookieAPI/Migrations/20260405211141_Added Age Property.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CookieAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedAgeProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<char>(
+                name: "Gender",
+                table: "User",
+                type: "character(1)",
+                nullable: true,
+                oldClrType: typeof(char),
+                oldType: "character(1)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "User",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "User");
+
+            migrationBuilder.AlterColumn<char>(
+                name: "Gender",
+                table: "User",
+                type: "character(1)",
+                nullable: false,
+                defaultValue: '\0',
+                oldClrType: typeof(char),
+                oldType: "character(1)",
+                oldNullable: true);
+        }
+    }
+}
